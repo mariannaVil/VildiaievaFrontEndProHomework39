e9b3edad1a3afa97b06b74afc990bcd695a4c6ce
mariannaVil <mariannavild@gmail.com>
2023-12-11 11:47:27 +0200

    "new changes to script"

--- script.js
+++ script.js
@@ -1,30 +1,46 @@
-function getWeather(q) {
-    var apiUrl = 'http://api.openweathermap.org/data/2.5/weather?q=' + q + '&units=metric&APPID=5d066958a60d315387d9492393935c19';
+const apiUrl = 'http://api.openweathermap.org/data/2.5/weather';
+const apiKey = '5d066958a60d315387d9492393935c19';
 
-        var newObject = new XMLHttpRequest();
+const cityElement = document.getElementById('q');
+const tempElement = document.getElementById('temp');
+const pressureElement = document.getElementById('pressure');
+const descriptionElement = document.getElementById('description');
+const humidityElement = document.getElementById('humidity');
+const speedElement = document.getElementById('speed');
+const degElement = document.getElementById('deg');
+const iconElement = document.getElementById('icon');
 
-        newObject.open('GET', apiUrl, true);
+function buildApiUrl(q) {
+    return `${apiUrl}?q=${q}&units=metric&APPID=${apiKey}`;
+}
 
-        newObject.onload = function () {
-            newObject.status === 200 
-            ? (function () {
-                
-                var weatherData = JSON.parse(newObject.responseText);
+function fetchWeatherData(q) {
+    const url = buildApiUrl(q);
 
-                document.getElementById('q').innerText = 'Місто: ' + weatherData.name;
-                document.getElementById('temp').innerText = 'Температура: ' + weatherData.main.temp + ' °C';
-                document.getElementById('pressure').innerText = 'Тиск: ' + weatherData.main.pressure + ' hPa';
-                document.getElementById('description').innerText = 'Опис: ' + weatherData.weather[0].description;
-                document.getElementById('humidity').innerText = 'Вологість: ' + weatherData.main.humidity + ' %';
-                document.getElementById('speed').innerText = 'Швидкість вітру: ' + weatherData.wind.speed + ' м/с';
-                document.getElementById('deg').innerText = 'Напрям у градусах: ' + weatherData.wind.deg + ' градусів';
-                document.getElementById('icon').src = 'http://openweathermap.org/img/w/' + weatherData.weather[0].icon + '.png';
+    return fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Помилка отримання даних про погоду');
+            }
+            return response.json();
+        });
+}
 
-            })()
-            : console.error('Помилка отримання даних про погоду');
-        }
-        
-        newObject.send();
-    }
+function renderWeatherData(weatherData) {
+    cityElement.innerText = 'Місто: ' + weatherData.name;
+    tempElement.innerText = 'Температура: ' + weatherData.main.temp + ' °C';
+    pressureElement.innerText = 'Тиск: ' + weatherData.main.pressure + ' hPa';
+    descriptionElement.innerText = 'Опис: ' + weatherData.weather[0].description;
+    humidityElement.innerText = 'Вологість: ' + weatherData.main.humidity + ' %';
+    speedElement.innerText = 'Швидкість вітру: ' + weatherData.wind.speed + ' м/с';
+    degElement.innerText = 'Напрям у градусах: ' + weatherData.wind.deg + ' градусів';
+    iconElement.src = 'http://openweathermap.org/img/w/' + weatherData.weather[0].icon + '.png';
+}
 
-    getWeather('Vinnytsia');
+function controller(city) {
+    fetchWeatherData(city)
+        .then(weatherData => renderWeatherData(weatherData))
+        .catch(error => console.error(error));
+}
+
+controller('Vinnytsia');
